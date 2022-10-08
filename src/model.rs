b0f10cb0b9286086f3744bc090054d7a441ca96f
liabri <liabri@tuta.io>
2022-10-08 13:37:23 +0200

    Remove serde deserialize with

--- src/model.rs
+++ src/model.rs
@@ -73,27 +73,3 @@ impl Article {
         })
     }
 }
-
-// fn md_to_html<'de, D>(deserializer: D) -> Result<String, D::Error> where D: serde::Deserializer<'de> {
-//     struct Markdown;
-
-//     impl<'de> de::Visitor<'de> for Markdown {
-//         type Value = String;
-
-//         fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-//             formatter.write_str("string")
-//         }
-
-//         fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> where E: de::Error {
-//             let mut options = Options::empty();
-//             options.insert(Options::ENABLE_STRIKETHROUGH);
-            
-//             let parser = Parser::new_ext(value, options);
-//             let mut html_output = String::new();
-//             html::push_html(&mut html_output, parser);
-//             Ok(html_output)
-//         }
-//     }
-
-//     deserializer.deserialize_any(Markdown)
-// }
